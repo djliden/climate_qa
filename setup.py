ea7d3ab5f0303ea34434b7f3bba694fd2214ae3a
Daniel Liden <djliden91@gmail.com>
2023-05-23 20:23:45 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="climate_qa",
+    version="0.1",
+    packages=find_packages(),
+    author="Daniel Liden",
+    author_email="djliden91@gmail.com",
+    description="A Python package for querying and summarizing major climate change documents",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/djliden/climate_qa",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',  # Update with your project's Python version requirement
+    install_requires=[
+    ],
+)
